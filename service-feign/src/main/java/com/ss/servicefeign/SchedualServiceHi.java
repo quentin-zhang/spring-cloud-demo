9e4012ef71512dc5f9aaa18ba4942ccd74ed7f69
zhangyin <zhangyin@zy.com>
2020-06-10 16:16:34 +0800

    add hytrix surpport

--- service-feign/src/main/java/com/ss/servicefeign/SchedualServiceHi.java
+++ service-feign/src/main/java/com/ss/servicefeign/SchedualServiceHi.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
     @GetMapping(value = "/hi")
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
