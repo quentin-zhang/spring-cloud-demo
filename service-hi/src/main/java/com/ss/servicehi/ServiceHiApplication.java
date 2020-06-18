f10f1c9f1f94bf59b708907285c9fb4250c2a5e1
zhangyin <zhangyin@zy.com>
2020-06-18 18:48:16 +0800

    nacos

--- service-hi/src/main/java/com/ss/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/ss/servicehi/ServiceHiApplication.java
@@ -1,9 +1,14 @@
 package com.ss.servicehi;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceHiApplication {
 
     public static void main(String[] args) {
@@ -21,7 +30,12 @@ public class ServiceHiApplication {
     String port;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
